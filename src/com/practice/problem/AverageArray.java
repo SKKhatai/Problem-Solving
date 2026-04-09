1bf13624a53ae19adc52939875f56ffbe1f13bd4
Sourav Kumar Khatai <9777238708skk@gmail.com>
2026-04-09 18:47:23 +0530

    found max and min value

--- src/com/practice/problem/AverageArray.java
+++ src/com/practice/problem/AverageArray.java
@@ -15,17 +15,27 @@ public class AverageArray {
         }
         int minindex=0;
         int maxindex=0;
+        int minvalue=Integer.MIN_VALUE;
+        int maxvalue=Integer.MAX_VALUE;
+
         int sum=0;
         int count=0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > arr[maxindex]) {
                 maxindex = i;
+                maxvalue=arr[maxindex];
             }
             if (arr[i] < arr[minindex]) {
                 // min=arr[i];
                 minindex = i;
+                minvalue=arr[minindex];
             }
+
         }
+        System.out.println("minimum index is " + minindex);
+       System.out.println("maximum index is "+ maxindex);
+       System.out.println("minimul value is "+minvalue);
+       System.out.println("maximum value is "+maxvalue);
         arr[minindex]=0;
         arr[maxindex]=0;
         for (int i = 0; i < arr.length; i++) {
